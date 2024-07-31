4f18faec6f6bd33c0e109d26edb3710bb02d3e96
ACJT123 <acjtacjt@gmail.com>
2024-08-01 00:14:26 +0800

    feat(component): add today-highlight component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,12 @@
 import Image from "next/image";
+import { data } from "./data";
+import Card from "./components/today-highlight/card";
+import TodayHighlight from "./components/today-highlight/today-highlight";
 
 export default function Home() {
-  return <main></main>;
+  return (
+    <main>
+      <TodayHighlight />
+    </main>
+  );
 }
