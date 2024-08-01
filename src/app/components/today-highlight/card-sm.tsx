189b5910d5fd02a6f6ea47f8860643acd26df7f5
ACJT123 <acjtacjt@gmail.com>
2024-08-01 11:48:53 +0800

    feat(component): update card component styling  (padding and border-radius)

--- src/app/components/today-highlight/card-sm.tsx
+++ src/app/components/today-highlight/card-sm.tsx
@@ -1,5 +1,5 @@
 import { ReactNode } from "react";
 
 export default function CardSm({ children }: { children: ReactNode }) {
-  return <div className="bg-[#111e25] rounded-lg">{children}</div>;
+  return <div className="bg-[#111e25] rounded-2xl p-4">{children}</div>;
 }
