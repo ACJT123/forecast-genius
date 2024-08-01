8c5d1862c7fe56dae3924ac0cdc5907271d79b71
ACJT123 <acjtacjt@gmail.com>
2024-08-01 11:57:40 +0800

    feat(component): add uv index component

---
+++ src/app/models/gauge.ts
@@ -0,0 +1,10 @@
+export const subArcData = [
+  { limit: 2, color: "#1e3646" },
+  { limit: 3, color: "#336B7E" },
+  { limit: 5, color: "#336B7E" },
+  { limit: 6, color: "#4691A6" },
+  { limit: 7, color: "#4691A6" },
+  { limit: 8, color: "#52c6df" },
+  { limit: 10, color: "#52c6df" },
+  { limit: 12, color: "#76d1ef" },
+];
