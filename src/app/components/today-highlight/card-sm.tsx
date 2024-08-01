561c33724370208b4990d3c117da187a577ec793
ACJT123 <acjtacjt@gmail.com>
2024-08-01 11:46:34 +0800

    feat(component): update card component styling

--- src/app/components/today-highlight/card-sm.tsx
+++ src/app/components/today-highlight/card-sm.tsx
@@ -1,5 +1,5 @@
 import { ReactNode } from "react";
 
 export default function CardSm({ children }: { children: ReactNode }) {
-  return <div className="bg-[#131c25] rounded-lg">{children}</div>;
+  return <div className="bg-[#111e25] rounded-lg">{children}</div>;
 }
