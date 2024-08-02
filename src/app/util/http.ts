92a06c4904a2dca4b50916236a8a0a0e291aa5e3
ACJT123 <acjtacjt@gmail.com>
2024-08-02 15:32:06 +0800

    feat(util): add http util

---
+++ src/app/util/http.ts
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+export const getData = async (url: string) => {
+  try {
+    const response = await axios.get(url);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const postData = async (url: string) => {
+  try {
+    const response = await axios.get(url);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
+};
