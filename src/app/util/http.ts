357156955ceb0e32c0f39aa5c7a47cf285960a8c
ACJT123 <acjtacjt@gmail.com>
2024-08-04 16:44:16 +0800

    fix(http): update method for postdata function

--- src/app/util/http.ts
+++ src/app/util/http.ts
@@ -1,5 +1,7 @@
 import axios from "axios";
 
+export const API_KEY = "&apikey=DRkIEgVxYkqsx0Ku4sgLZc3cLCRfplqF";
+
 export const getData = async (url: string) => {
   try {
     const response = await axios.get(url);
@@ -9,9 +11,9 @@ export const getData = async (url: string) => {
   }
 };
 
-export const postData = async (url: string) => {
+export const postData = async (url: string, data: any) => {
   try {
-    const response = await axios.get(url);
+    const response = await axios.post(url, data);
     return response.data;
   } catch (error) {
     console.error(error);
