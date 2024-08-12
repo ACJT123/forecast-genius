d5e6115a430b245e2bfe7b702e4f1537d94d1924
ACJT123 <acjtacjt@gmail.com>
2024-08-12 17:11:59 +0800

    feat(wind status): pluck api

--- src/app/components/today-highlight/wind-status.tsx
+++ src/app/components/today-highlight/wind-status.tsx
@@ -2,24 +2,30 @@ import { data } from "@/app/data";
 import Card from "./card";
 import WindStatusChart from "./wind-status-chart";
 import { DateTime } from "luxon";
+import { useWeather } from "@/app/context/WeatherContext";
 
 export default function WindStatus() {
+  const { forecastData } = useWeather();
+
+  const data = forecastData?.forecastData.timelines.daily[0];
+
   return (
     <Card>
       <div className="flex flex-col items-center">
         <div className="text-start w-full">Wind Status</div>
 
-        <WindStatusChart />
+        <WindStatusChart data={forecastData?.forecastData.timelines.hourly} />
 
         <div className="flex items-baseline justify-between w-full">
           <div>
             <span className="text-[40px]">
-              {data.timelines.hourly[0].values.windSpeed?.toFixed(2)}
+              {data?.values.windSpeedAvg.toFixed(2)}
             </span>
             <span className="text-xs">&nbsp;km/h</span>
           </div>
+
           <div className="text-xl">
-            {DateTime.fromISO(data.timelines.hourly[0].time).toFormat("t")}
+            {/* {DateTime.fromISO(data.timelines.hourly[0].time).toFormat("t")} */}
           </div>
         </div>
       </div>
